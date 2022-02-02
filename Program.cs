8cc91e1ddb4aea8907c0d863be9d31e155cb2d8e
evgeny <evgeniu_k99@mail.ru>
2022-02-02 13:19:39 +0300

    task completed

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string[] array =
+{
+    "run",
+    "sleep",
+    "VSC",
+    "best",
+    "fire",
+    "car"
+};
+
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
